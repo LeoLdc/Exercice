e67aad81f17218328587567cacc4474b8e963eb4
Leo <ledocq.leo@laposte.net>
2018-06-21 12:16:35 +0200

    m

---
+++ ex1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <meta charset="utf-8">
+   <link rel="stylesheet" type="text/css" href="style.css">
+ </head>
+ <body>
+  <main>
+    <form method="post" action="cible.php" target="_blank">
+      <input type="text" name="email">
+      <input type="text" name="text">
+      <input type="submit">
+    </form>
+  </main>
+ </body>
+</html>
